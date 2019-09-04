800f71e8a9abf0a8bb943544f503027cbe729ae3
eecarres@gmail.com <ecarres@hemav.com>
2019-09-04 12:35:11 +0200

    Add persistence to postgresql data in docker-compose + Add Brand and Language

--- backend/orgasmi_backend/models.py
+++ backend/orgasmi_backend/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 
+class Language(models.Model):
+
+    abbreviation = models.CharField(max_length=3, unique=True)
+
+    name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Brand(models.Model):
+
+    name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class User(models.Model): 
 
     name = models.CharField("Name",max_length=60)
@@ -15,6 +31,8 @@ class User(models.Model):
     mail = models.EmailField("E-mail", unique = True)
 
     username = models.CharField("Username", max_length=60)
+
+    language = models.ForeignKey(Language, on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return f"{self.name} {self.surname}"
@@ -35,8 +53,12 @@ class Professional(models.Model):
     mail = models.EmailField("E-mail", unique = True)
     
     username = models.CharField("Username", max_length=60)
+
+    languages = models.ManyToManyField("Language", related_name="professional_languages",blank=True)
+
+    brands = models.ManyToManyField("Brand", related_name="professional_brands",blank=True)
     
-    def __str__(self):
+    def __str__(self):  
         return self.name
 
 
@@ -45,6 +67,8 @@ class Service(models.Model):
     name = models.CharField("Name",max_length=120)
 
     description = models.TextField("Description")
+
+    price = models.FloatField(default=0.0)
 
     class Meta:
         verbose_name = "Service"
@@ -79,7 +103,10 @@ class Order(models.Model):
 
     scheduled_datetime = models.DateTimeField("Scheduled date")
 
-    user_review = models.ForeignKey(Review, on_delete=models.PROTECT)
+    user_review = models.ForeignKey(Review, blank=True, null=True, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"{self.user} - {self.professional} on {self.scheduled_datetime}"
 
 
 
